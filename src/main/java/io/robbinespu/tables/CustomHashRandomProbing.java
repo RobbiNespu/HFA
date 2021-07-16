ebc3cafc75615bf2f0b02c8c6aa12ecea292f1c5
Robbi Nespu <robbinespu@gmail.com>
2021-07-16 13:57:34 +0800

    Draft note so I wont forgot the ideas

---
+++ src/main/java/io/robbinespu/tables/CustomHashRandomProbing.java
@@ -0,0 +1,14 @@
+package io.robbinespu.tables;
+
+public class CustomHashRandomProbing {
+    /* Todo: answers are duplicate, maybe it have same index?
+      for example eg: {q1,r1} = {y} and {q1,r2} = {y} , which mean the both answered Y on Question 1
+      so we should hash(answers) and copy the index on arrays
+
+    --------
+    |___Y__| <-- store (q1,r1) and (q1,r2) here (index : 0) == array answersQ1 = [ 0,0,1.....]
+    |___N__| <--- q1,r3
+    |__NIL_|
+
+    */
+}
